9be9e3456e047086033beaf0091ee2a366e3bd41
gooozooo <ella0729@gachon.ac.kr>
2025-12-11 19:39:29 +0900

    initial commit

---
+++ src/main/java/com/example/heydibe/report/domain/MonthlyReport.java
@@ -0,0 +1,5 @@
+package com.example.heydibe.report.domain;
+
+public class MonthlyReport {
+    // Entity 정의 예정
+}
---
+++ src/main/java/com/example/heydibe/report/service/ReportService.java
@@ -0,0 +1,12 @@
+package com.example.heydibe.report.service;
+
+public interface ReportService {
+    Object getAvailableMonths();
+    Object getMonthlyEmotions(String yearMonth);
+    Object getMonthlyTopics(String yearMonth);
+    Object getMonthlyPreferences(String yearMonth);
+    Object getMonthlyActivities(String yearMonth);
+    Object getMonthlyInsights(String yearMonth);
+    Object getMonthlyCalendar(String yearMonth);
+    Object getMonthlyReminder(String yearMonth);
+}
